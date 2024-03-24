a03c8c3166265c6199c71ba95045227638393937
Antoine Després <58844676+antoinedespres@users.noreply.github.com>
2024-03-24 16:47:44 +0900

    Revert "fix: bcrypt encoder"
    This reverts commit e5da11a85a8a4d6c37b73b243fc836b84c59b435.

--- src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
+++ src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
@@ -41,8 +41,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /** Security configuration for WebGoat. */
@@ -109,7 +108,7 @@ public class WebSecurityConfig {
   }
 
   @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
+  public NoOpPasswordEncoder passwordEncoder() {
+    return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
   }
 }
